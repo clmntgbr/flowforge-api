268193fd0a155cb9cd920c735ff3e83a2f292883
Clément <33451311+clmntgbr@users.noreply.github.com>
2026-07-02 17:42:27 +0200

    Add IsSecret, DefaultValue, and LastValue fields to VariableResponse
    - Updated the VariableResponse struct to include new fields: `IsSecret`, `DefaultValue`, and `LastValue`, enhancing the representation of variable details.
    - Adjusted the NewVariableResponse function to populate the new fields from the entity, improving data handling in API responses.

--- presenter/variable.go
+++ presenter/variable.go
@@ -5,11 +5,14 @@ import (
 )
 
 type VariableResponse struct {
-	ID     string             `json:"id"`
-	Name   string             `json:"name"`
-	Path   string             `json:"path"`
-	StepID string             `json:"stepId"`
-	Step   StepDetailResponse `json:"step"`
+	ID           string             `json:"id"`
+	Name         string             `json:"name"`
+	Path         string             `json:"path"`
+	StepID       string             `json:"stepId"`
+	Step         StepDetailResponse `json:"step"`
+	IsSecret     bool               `gorm:"default:false" json:"isSecret"`
+	DefaultValue string             `gorm:"null" json:"defaultValue"`
+	LastValue    string             `gorm:"null" json:"lastValue"`
 }
 
 func NewVariableResponses(variables []entity.Variable) []VariableResponse {
@@ -22,10 +25,13 @@ func NewVariableResponses(variables []entity.Variable) []VariableResponse {
 
 func NewVariableResponse(variable entity.Variable) VariableResponse {
 	return VariableResponse{
-		ID:     variable.ID.String(),
-		Name:   variable.Name,
-		Path:   variable.Path,
-		StepID: variable.StepID.String(),
-		Step:   NewStepDetailResponse(variable.Step),
+		ID:           variable.ID.String(),
+		Name:         variable.Name,
+		Path:         variable.Path,
+		StepID:       variable.StepID.String(),
+		Step:         NewStepDetailResponse(variable.Step),
+		IsSecret:     variable.IsSecret,
+		DefaultValue: variable.DefaultValue,
+		LastValue:    variable.LastValue,
 	}
 }
